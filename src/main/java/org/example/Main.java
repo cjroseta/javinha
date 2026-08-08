93d9a43ccf59775022762a9939fbad9e36111033
Cláudio Roseta <claudioroseta@gmail.com>
2026-08-08 15:42:48 +0200

    Primeiro programa: Hello World com sintaxe classica do main()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome! Maratonando o Java com o JAVINHA.");
+        System.out.println("Hello world and welcome! Maratonando o Java com o JAVINHA.");
     }
 }
